7ea0b27c1b9724f568950c7874ea0ad2d6047db6
JujuProdGames <speedcuber25@outlook.com>
2023-12-01 18:17:55 -0600

    12-1-23 Springtrap Scene + Cam Static + Call Skip!
    WOOHOO!
    
    poggity pog pog :fire: :thumbsup: <3 :)

--- FNAF 2D/scripts/characters/animatronics/power_off_sequence.gd
+++ FNAF 2D/scripts/characters/animatronics/power_off_sequence.gd
@@ -11,15 +11,18 @@ signal jumpscare_played
 @export var timer_until_animatronic_footstep : Timer
 @export var time_until_freddy_flicker_range : Vector2 = Vector2(7.5, 12.5)
 @export var timer_until_freddy_flicker : Timer
-@export var time_until_power_darkness_range : Vector2 = Vector2(5, 15)
+@export var time_until_power_darkness_range : Vector2 = Vector2(8, 16)
 @export var timer_until_power_darkness : Timer
-@export var time_until_jumpscare_range : Vector2 = Vector2(4, 10)
+@export var time_until_animatronic_footstep_darkness_range : Vector2 = Vector2(2.5, 4.5)
+@export var timer_until_animatronic_footstep_darkness : Timer
+@export var time_until_jumpscare_range : Vector2 = Vector2(4, 12)
 @export var timer_until_jumpscare : Timer
 
 func _ready():
 	timer_until_animatronic_footstep.wait_time = randf_range(time_until_animatronic_footstep_range.x, time_until_animatronic_footstep_range.y)
 	timer_until_freddy_flicker.wait_time = randf_range(time_until_freddy_flicker_range.x, time_until_freddy_flicker_range.y)
 	timer_until_power_darkness.wait_time = randf_range(time_until_power_darkness_range.x, time_until_power_darkness_range.y)
+	timer_until_animatronic_footstep_darkness.wait_time = randf_range(time_until_animatronic_footstep_darkness_range.x, time_until_animatronic_footstep_darkness_range.y)
 	timer_until_jumpscare.wait_time = randf_range(time_until_jumpscare_range.x, time_until_jumpscare_range.y)
 
 func increment_power_off_stage():
--- FNAF 2D/scripts/monitor/monitor_screen.gd
+++ FNAF 2D/scripts/monitor/monitor_screen.gd
@@ -29,9 +29,9 @@ func set_active_state(active_state : bool):
 	
 	if active_state: active_state_on.emit()
 	else: active_state_off.emit()
-	for child in nodes_to_set_active:
-		child.set_process(active_state)
-		child.visible = active_state
+#	for child in nodes_to_set_active:
+#		child.set_process(active_state)
+#		child.visible = active_state
 
 func set_active_state_permanently(active_state : bool): 
 	set_active_state(active_state)
@@ -61,7 +61,9 @@ func set_animatronic_pos(_animatronic_name : String, _cam_name : String, _num_ti
 		monitor_script.set_animatronic_pos(_animatronic_name, _cam_name, _num_times_been_to_cam)
 
 func animatronic_moved_from(_animatronic_name : String, _cam_name_from : String):
+	if !is_focused: return
 	if _cam_name_from == current_cam_scene: animatronic_moved_out_of_camera_queue.emit()
 	
 func animatronic_moved_to(_animatronic_name : String, _cam_name_to : String, _num_times_to_thing : int = -1):
+	if !is_focused: return
 	if _cam_name_to == current_cam_scene: animatronic_moved_in_camera_queue.emit()
